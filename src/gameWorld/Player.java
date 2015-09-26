6c91a6b5bd25d92b7c076f99086048506cf0929b
chrisjaques <chrisszpetnar@gmail.com>
2015-09-26 13:16:34 +1200

    Added direction

--- src/gameWorld/Player.java
+++ src/gameWorld/Player.java
@@ -4,6 +4,7 @@ import java.awt.Point;
 
 public class Player {
 	private Point myLocation;
+	private String direction; //String representing which direction player is facing
 	
 	public Player(Point loc){
 		this.myLocation = loc;
@@ -14,9 +15,30 @@ public class Player {
 	}
 	
 	public void setMyLocation(Point myLocation) {
+		int oldX = this.myLocation.x;
+		int oldY = this.myLocation.y;
+		int newX = myLocation.x;
+		int newY = myLocation.y;
+		if (oldX == newX && newY > oldY){
+			setDirection("North");
+		} else if (oldX == newX && newY < oldY){
+			setDirection("South");
+		} else if (oldX > newX && newY == oldY){
+			setDirection("West");
+		} else {
+			setDirection("East");
+		}
 		this.myLocation = myLocation;
 	}
 	
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
 	@Override
 	public String toString() {
 		return "p";
