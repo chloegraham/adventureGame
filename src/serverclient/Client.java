461c2dd286ab90b5037059abbc96c80cd5e6a8e3
xxPicasso <ben@moregolf.co.nz>
2015-10-01 15:11:47 +1300

    Added new Server.class and Client.class

--- src/serverclient/Client.java
+++ src/serverclient/Client.java
@@ -1,48 +1,110 @@
 package serverclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 import userinterface.UserInterface;
 
-public class Client {
+public class Client implements Runnable {
+	private String host;
+	private int port;
+	private Socket connection;
+	private InetAddress address;
+	
+	private DataOutputStream output;
+	private DataInputStream input;
+	
 	private UserInterface ui;
 	
 	public Client() {
-		ui = new UserInterface();
+		ui = new UserInterface(this);
 	}
 	
-	// TEMP
-	private int uid = 0;
-	// TEMP
-	public int getUID() {
-		return uid;
+	public void run() {
+	    host = "localhost";  // Define a host server
+	    port = 19999;	// Define a port
+	 
+	    try {
+	    	System.out.println("CLIENT: SocketClient initialized");	// Try and connect to the Server
+	        address = InetAddress.getByName(host);	// Obtain an address object of the server
+	        connection = new Socket(address, port); // Establish a socket connection 
+	        System.out.println("CLIENT: I've connected to the Server :)"); 
+	        
+	    	output = new DataOutputStream(connection.getOutputStream());
+	    	input = new DataInputStream(connection.getInputStream());
+ 
+	    	while (input.available() == 0) {
+	    	}
+	    	// Get initial GameState from Server
+	    	System.out.println("CLIENT: Waiting for initial GameState from Server");
+			String s = input.readUTF();
+			GameState gs = new GameState(s);
+			ui.redraw(gs.getLayerStatic());
+			System.out.println("CLIENT: I should have printed the initial GameState by now.");
+	    	
+	   } catch (IOException f) {
+		   System.out.println("IOException: " + f);
+	   } 
 	}
 	
-	
-	
-	
-	
-	public int getAction() {
-		return  ui.getAction();
-	}
-	
-	public void updateClient(char[][] gameWorld) {
-		ui.redraw(gameWorld);
+	public void passClientAction(int action) {
+		try {
+			while (output == null) {
+			}
+			
+			output.writeInt(action);
+			System.out.println("CLIENT: -- Action sent --- It was (" + action + ") now wait");
+			
+			System.out.println("CLIENT: Waiting for Game State from the Server");
+			String s = input.readUTF();
+			GameState gs = new GameState(s);
+			ui.redraw(gs.getLayerStatic());
+			System.out.println("CLIENT: ------------ Here is the Game State from the Server (" + s + ") thoughts?");
 		
-		char[][] testLevel =   {{'w','w','w','w'},
-                {'w','e','e','e'},
-                {'w','e','e','e'},
-                {'w','e','e','e'}};
-    	
-    	char[][] testObjects =   {{'n','n','n','n'},
-    			{'n','n','n','n'},
-    			{'n','c','n','n'}, // Notice i'm using 'n' for nothing
-    			{'n','n','n','n'}};
-    	
-    	char[][] testMoveables =  {{'n','n','n','n'},
-    			{'n','n','n','p'},
-    			{'n','n','n','n'},
-    			{'n','n','n','n'}};
-    	
-		// This method will do same shit as redraw, but from multiple layers
-		//ui.redrawFromLayers(testLevel, testObjects, testMoveables);
+		} catch (IOException f) {
+			System.out.println("IOException: " + f);
+		}
 	}
 }
+
+	
+	
+	
+	
+	
+//	// TEMP
+//	private int uid = 0;
+//	// TEMP
+//	public int getUID() {
+//		return uid;
+//	}
+//	
+//	public int getAction() {
+//		return  ui.getAction();
+//	}
+//	
+//	public void updateClient(char[][] gameWorld) {
+//		ui.redraw(gameWorld);
+//		
+//		char[][] testLevel =   {{'w','w','w','w'},
+//                {'w','e','e','e'},
+//                {'w','e','e','e'},
+//                {'w','e','e','e'}};
+//    	
+//    	char[][] testObjects =   {{'n','n','n','n'},
+//    			{'n','n','n','n'},
+//    			{'n','c','n','n'}, // Notice i'm using 'n' for nothing
+//    			{'n','n','n','n'}};
+//    	
+//    	char[][] testMoveables =  {{'n','n','n','n'},
+//    			{'n','n','n','p'},
+//    			{'n','n','n','n'},
+//    			{'n','n','n','n'}};
+//    	
+//		// This method will do same shit as redraw, but from multiple layers
+//		//ui.redrawFromLayers(testLevel, testObjects, testMoveables);
+//	}
+//}
--- src/serverclient/Server.java
+++ src/serverclient/Server.java
@@ -1,27 +1,88 @@
 package serverclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import gameWorld.GameLogic;
 
 public class Server implements Runnable {
-	private Client client;
 	private GameLogic logic;
 	
+	private static ServerSocket socket1;
+	private final static int port = 19999;
+	private static Socket connection;
+ 
+	private DataOutputStream output;
+	private DataInputStream input;
+	
 	public Server(GameLogic logic) {
-		this.client = new Client();
 		this.logic = logic;
 	}
 	
 	public void run() {
-		while (true) {
-			// Get int.action from Client and tell GameLogic
-			int action = client.getAction();
-			int uid = client.getUID();
-			logic.handleAction(action, uid);
-			// Get char[][].gameWorld and send it to the Client/Renderer
-			char[][] gameWorld = logic.getGameWorld();
-			client.updateClient(gameWorld);
-			
-			
-		}
-	}
+	    try{
+	      socket1 = new ServerSocket(port);
+	      System.out.println("SERVER: SingleSocketServer Initialized");
+	      
+	      	  // Do nothing until connected to ONE client
+	    	  System.out.println("SERVER: Waiting for Client");
+	          connection = socket1.accept();
+	          System.out.println("SERVER: Client found me :)");
+
+	          input = new DataInputStream(connection.getInputStream());
+	          output = new DataOutputStream(connection.getOutputStream());
+		         
+	          System.out.println("SERVER: Sending Client initial GameState");
+	          char[][] gs1 = logic.getGameWorld();
+	          GameState gs2 = new GameState(gs1, gs1, gs1);
+	          String gs3 = gs2.getGameStateString();
+	          output.writeUTF(gs3);
+	          System.out.println("SERVER: I've sent the INITIAL GameState.");
+		          
+	          // Every 5 seconds check for an Action from Client and then send Client the GameState  
+	          while (true) {
+	        	  
+	        	  // 5 second timer
+	        	  try {	Thread.sleep(5000);	} catch (Exception e) {}
+	        	  
+	        	  // Check for an Action
+	        	  System.out.println("SERVER: Checking for an action");
+		          int action = input.readInt();
+		          logic.handleAction(action, 999);
+		          System.out.println("SERVER: I sent action: - " + action + " to the Client.");
+		      
+		          // Send Client the GameState
+		          System.out.println("SERVER: Sending Client the GameState");
+		          char[][] gsArray = logic.getGameWorld();
+		          GameState gs = new GameState(gsArray, gsArray, gsArray);
+		          String gameState = gs.getGameStateString();
+		          output.writeUTF(gameState);
+		          System.out.println("SERVER: I've sent the GameState. Lets see if Client gets it");
+	          }
+		          
+	      } catch (IOException e) {}
+		      	try {
+		      		connection.close();
+		        } catch (IOException e) {}
+    }
 }
+
+//public Server(GameLogic logic) {
+//	this.client = new Client();
+//	this.logic = logic;
+//}
+//
+//public void runaa() {
+//	while (true) {
+//		// Get int.action from Client and tell GameLogic
+//		int action = client.getAction();
+//		int uid = client.getUID();
+//		logic.handleAction(action, uid);
+//		// Get char[][].gameWorld and send it to the Client/Renderer
+//		char[][] gameWorld = logic.getGameWorld();
+//		client.updateClient(gameWorld);
+//	}
+//}
