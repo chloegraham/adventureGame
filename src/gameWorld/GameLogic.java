86135a02b5953c5612430f58077a72758df59309
xxPicasso <ben@moregolf.co.nz>
2015-10-13 21:20:40 +1300

    Typo on down cast

--- src/gameWorld/GameLogic.java
+++ src/gameWorld/GameLogic.java
@@ -184,7 +184,7 @@ public class GameLogic {
 		// If Tile is a Door
 		// The Passable check above ensure LevelDoor is also Passable
 		if (nextTile instanceof Door) {
-			if ( ((DoorLevel)nextTile).isPassable() ) {
+			if ( ((Passable)nextTile).isPassable() ) {
 				Doors d = DoorTransfers.transfer(player.getStageID(), player.getRoomID(), nextLoc);
 				gameWorld.removePlayers();
 				player.setLocation(d.getStage(), d.getRoom(), d.getLocation());
